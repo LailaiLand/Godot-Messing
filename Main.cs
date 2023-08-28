22636505ca95321b3fa2874c9fb59fc9dde069a6
Nikolaipe <nikolaipe@getacademy.no>
2023-08-28 10:38:26 +0200

    Trying out tilesheets because I couldn't figure out how to manually add collisions to a sprite

--- Main.cs
+++ Main.cs
@@ -10,6 +10,11 @@ public partial class Main : Node
 		Poop poop = SpawnPoop.Instantiate<Poop>();
 		var biterLocation = GetNode<Biter>("Biter").Position;
 		poop.Position = biterLocation;
+		//GetNode<Biter>("Biter").ZIndex = 5;
+		//GetNode<Crawler>("Crawler").ZIndex = 3;
+		//GetNode<Map>("Map").ZIndex = 0;
+		//GetNode<Stage>("Stage").ZIndex = 0;
+		//poop.ZIndex = 2;
 		AddChild(poop);
 	}
 
