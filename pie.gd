ef74fc1621d5c6ab4654f153ccc4e2c95d71d155
Nikolaipe <nikolaipe@getacademy.no>
2023-10-06 12:17:11 +0200

    Spawn ratio for upgrades were a little too generous

--- pie.gd
+++ pie.gd
@@ -32,7 +32,7 @@ func _on_body_entered(body):
 
 func _random_heart_spawn():
 	var random_num = randf_range(0, 1)
-	if random_num <= 0.3:
+	if random_num <= 0.1:
 		_random_item()
 
 func _random_item():
